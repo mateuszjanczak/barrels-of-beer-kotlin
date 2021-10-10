eec97b14fa1cce9e4d8db68edbfe237a76a713c3
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2021-10-11 01:19:56 +0200

    BoBs-10 Set up integration tests, fix unit tests startup

---
+++ src/integration-test/groovy/com.mateuszjanczak.barrelsofbeer/ITestMath.groovy
@@ -0,0 +1,18 @@
+package com.mateuszjanczak.barrelsofbeer
+
+import spock.lang.Specification
+
+class ITestMath extends Specification {
+
+    def "1 + 1 = 2"() {
+        given:
+        def a = 1
+        def b = 1
+
+        when:
+        def result = a + b
+
+        then:
+        result == 2
+    }
+}
