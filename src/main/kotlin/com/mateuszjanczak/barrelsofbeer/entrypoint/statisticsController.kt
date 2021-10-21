967c3219b3d58fbea6900bdd42af3a6730d8fa79
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2021-10-21 19:11:57 +0200

    BoBs-41 Add statistics

--- src/main/kotlin/com/mateuszjanczak/barrelsofbeer/entrypoint/statisticsController.kt
+++ src/main/kotlin/com/mateuszjanczak/barrelsofbeer/entrypoint/statisticsController.kt
@@ -1,14 +1,20 @@
 package com.mateuszjanczak.barrelsofbeer.entrypoint
 
 import com.mateuszjanczak.barrelsofbeer.domain.data.dto.Ranking
+import com.mateuszjanczak.barrelsofbeer.domain.data.dto.Statistics
 import com.mateuszjanczak.barrelsofbeer.domain.service.StatisticsService
 import com.mateuszjanczak.barrelsofbeer.entrypoint.StatisticsEndpoints.RANKING
+import com.mateuszjanczak.barrelsofbeer.entrypoint.StatisticsEndpoints.STATISTICS
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.ResponseEntity
 import org.springframework.http.ResponseEntity.ok
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+
 
 @RestController
 @CrossOrigin
@@ -20,8 +26,18 @@ class StatisticsController(
     @GetMapping(RANKING)
     fun getRanking(): ResponseEntity<List<Ranking>> =
         ok(statisticsService.getRanking())
+
+    @GetMapping(STATISTICS)
+    fun getStatistics(
+        @PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm") from: LocalDateTime,
+        @PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm") to: LocalDateTime,
+        @PathVariable interval: Long
+    ): ResponseEntity<List<Statistics>> =
+        ok(statisticsService.getStatistics(from, to, interval))
+
 }
 
 object StatisticsEndpoints {
     const val RANKING = "/ranking"
+    const val STATISTICS = "/statistics/from/{from}/to/{to}/interval/{interval}"
 }
