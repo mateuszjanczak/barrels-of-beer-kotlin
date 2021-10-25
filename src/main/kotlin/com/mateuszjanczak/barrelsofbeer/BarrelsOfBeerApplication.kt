7bed526388c5a01530fc3a0bb5617acb45c0f922
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2021-10-25 21:36:09 +0200

    BoBs-51 Configure Spring to self-host React app

--- src/main/kotlin/com/mateuszjanczak/barrelsofbeer/BarrelsOfBeerApplication.kt
+++ src/main/kotlin/com/mateuszjanczak/barrelsofbeer/BarrelsOfBeerApplication.kt
@@ -2,8 +2,10 @@ package com.mateuszjanczak.barrelsofbeer
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class BarrelsOfBeerApplication
 
 fun main(args: Array<String>) {
